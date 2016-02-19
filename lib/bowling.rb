dc34dc3aa5f4a39f534f450d66c2b3496f5d48c1
Pedro Pimentel <zukunft@gmail.com>
2016-02-19 16:30:31 +0800

    on going refactoring

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -1,28 +1,36 @@
 class Bowling
+  NUM_OF_FRAMES = 10
   attr_accessor :rolls
+
+  def initialize
+    @current_roll_index = 0
+  end
 
   def score
     sum = 0
-    current_roll = 0
 
-    10.times do
-      if strike?(current_roll)
-        sum += 10 + @rolls[current_roll + 1].to_i + @rolls[current_roll + 2].to_i
-        current_roll += 1
-      elsif @rolls[current_roll].to_i + @rolls[current_roll + 1].to_i == 10
-        sum += 10 + @rolls[current_roll + 2].to_i
-        current_roll += 2
+    game_rolls.each do
+      if strike?
+        sum += 10 + @rolls[@current_roll_index + 1].to_i + @rolls[@current_roll_index + 2].to_i
+        @current_roll_index += 1
+      elsif @rolls[@current_roll_index].to_i + @rolls[@current_roll_index + 1].to_i == 10
+        sum += 10 + @rolls[@current_roll_index + 2].to_i
+        @current_roll_index += 2
       else
-        sum += @rolls[current_roll].to_i + @rolls[current_roll + 1].to_i
-        current_roll += 2
+        sum += @rolls[@current_roll_index].to_i + @rolls[@current_roll_index + 1].to_i
+        @current_roll_index += 2
       end
     end
     sum
   end
 
 private
-  def strike?(current_roll)
-    @rolls[current_roll] == 10
+  def strike?
+    @rolls[@current_roll_index] == 10
+  end
+
+  def game_rolls
+    @rolls.take(NUM_OF_FRAMES)
   end
 
 end
